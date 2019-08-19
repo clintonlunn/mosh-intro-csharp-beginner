4c26a020e7485174f2352c290f9b7627548b13db
Clinton Lunn <clintonlunn@Clintons-MacBook-Pro.local>
2019-08-19 14:00:44 -0400

    enum solution

--- enumFun/enumFun/Program.cs
+++ enumFun/enumFun/Program.cs
@@ -2,11 +2,30 @@
 
 namespace enumFun
 {
+    enum ShippingMethod
+    {
+
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+
+            var methodId = (ShippingMethod)method;
+            Console.WriteLine(methodId);
+
+
+            Console.WriteLine(method.ToString());
+
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+            Console.WriteLine(methodName);
         }
     }
 }
