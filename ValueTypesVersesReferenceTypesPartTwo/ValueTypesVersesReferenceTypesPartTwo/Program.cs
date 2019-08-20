18b117dcb8aa389e86bfab6dac3770fdc141eadb
Clinton Lunn <clintonlunn@Clintons-MacBook-Pro.local>
2019-08-19 22:01:48 -0400

    final solution for ref ttypes vs value types

--- ValueTypesVersesReferenceTypesPartTwo/ValueTypesVersesReferenceTypesPartTwo/Program.cs
+++ ValueTypesVersesReferenceTypesPartTwo/ValueTypesVersesReferenceTypesPartTwo/Program.cs
@@ -2,11 +2,33 @@
 
 namespace ValueTypesVersesReferenceTypesPartTwo
 {
-    class MainClass
+
+    public class Person
+    {
+        public int Age;
+    }
+
+    public class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var number = 1;
+            Increment(number);
+            Console.WriteLine(number);
+
+            var person = new Person() { Age = 20 };
+            MakeOld(person);
+            Console.WriteLine(person.Age);
+        }
+
+        public static void Increment (int number)
+        {
+            number += 10;
+        }
+
+        public static void  MakeOld (Person person)
+        {
+            person.Age += 10;
         }
     }
 }
